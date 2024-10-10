d6e2e996a287778aae5481079d583f8a503fdba9
nileshk1901 <nileshkush1901@gmail.com>
2024-10-10 10:15:45 +0530

    deployment issue

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ mongoose
 app.use("/crypto", cryptoRoutes);
 
 // Start the server
-const PORT = process.env.PORT||3000;
+const PORT = process.env.PORT||5000;
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 	// Start the background job immediately
